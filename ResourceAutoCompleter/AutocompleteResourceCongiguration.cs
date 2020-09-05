80aa1fd8930e5c152168605df42cedc771abf5db
Dmitry YERMACHENKO <d.yermachenko@gmail.com>
2020-09-05 23:38:50 +0200

    Creating autocemplete resources writer

---
+++ ResourceAutoCompleter/AutocompleteResourceCongiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResourceAutoCompleter {
+    public class AutocompleteResourceCongiguration {
+        public string FileName;
+
+    }
+}
---
+++ ResourceAutoCompleter/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+
+namespace ResourceAutoCompleter {
+    public class AutoCompleterStringLocalizer: IStringLocalizer  {
+        private readonly IStringLocalizer _RealLocalizer;
+        private readonly IConfiguration _Configuration;
+        private readonly CultureInfo _Culture;
+        private readonly string _ResourceName;
+
+        public AutoCompleterStringLocalizer(
+            IStringLocalizer realLocalizer,
+            IConfiguration configuration,
+            CultureInfo culture,
+            string resourceName) {
+            _RealLocalizer = realLocalizer;
+            _Configuration = configuration;
+            _Culture = culture;
+            _ResourceName = resourceName;
+        }
+
+        public LocalizedString this[string name] => throw new NotImplementedException();
+
+        public LocalizedString this[string name, params object[] arguments] => throw new NotImplementedException();
+
+        public IEnumerable<LocalizedString> GetAllStrings(bool includeParentCultures) {
+            throw new NotImplementedException();
+        }
+
+        public IStringLocalizer WithCulture(CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        
+
+
+    }
+}
