b9ac34ece98d752afdd5dc8a1a79dcab641153bc
Dmitry YERMACHENKO <d.yermachenko@gmail.com>
2020-10-19 12:09:28 +0200

    Controller added

---
+++ leave-management/Controllers/TestExamplesController.cs
@@ -0,0 +1,35 @@
+﻿using LeaveManagement.Code.CustomLocalization;
+using LeaveManagement.PasswordGenerator;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Controllers {
+    public class TestExamplesController : Controller {
+        private readonly Contracts.ILeaveManagementUnitOfWork _UnitOfWork;
+        private readonly UserManager<IdentityUser> _UserManager;
+        private readonly IStringLocalizer _StringLocalizer;
+        private readonly IPasswordGenerator _PasswordGenerator;
+        private readonly IEmailSender _EmailSender;
+
+        public TestExamplesController(
+            Contracts.ILeaveManagementUnitOfWork unitOfWork,
+            UserManager<IdentityUser> userManager,
+            ILeaveManagementCustomLocalizerFactory localizerFactory,
+            IPasswordGenerator passwordGenerator,
+            IEmailSender emailSender
+            ) {
+            _UnitOfWork = unitOfWork;
+            _UserManager = userManager;
+            _StringLocalizer = localizerFactory.Create(this.GetType());
+            _PasswordGenerator = passwordGenerator;
+            _EmailSender = emailSender;
+        }
+
+    }
+}
